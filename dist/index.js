37a2223d92812da4c565e8c6b2f363d6333a4aa5
rinostroh@gmail.com <rinostroh@gmail.com>
2017-08-04 12:18:56 -0400

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,6 @@ var port = process.env.PORT || 3000;
 _http2.default.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
 console.log('Server running at port:' + port);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
 console.log(`Server running at port:${port}`);
