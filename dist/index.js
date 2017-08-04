30258ab1f18a04d62b1ea6bdb7b6838239604618
rinostroh@gmail.com <rinostroh@gmail.com>
2017-08-04 12:06:28 -0400

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,11 @@ var _http2 = _interopRequireDefault(_http);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var port = process.env.PORT || 3000;
+
 _http2.default.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
-}).listen(3000, '127.0.0.1');
+}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at port:' + port);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import http from 'http';
 
+const port = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');
-}).listen(3000, '127.0.0.1');
+}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log(`Server running at port:${port}`);
