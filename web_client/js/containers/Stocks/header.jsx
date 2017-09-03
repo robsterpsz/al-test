29a0c4303b15a007c59754496b8c6dac3b15c246
rinostroh@gmail.com <rinostroh@gmail.com>
2017-09-03 14:07:34 -0300

    fixin header time format

--- web_client/js/containers/Stocks/header.jsx
+++ web_client/js/containers/Stocks/header.jsx
@@ -25,7 +25,7 @@ export default class StockHeader extends Component {
     let message = lastUpdate && `Last Update: ${new Date(lastUpdate * 1000).toLocaleString()}` || '';
 
     if (!market.isOpen) {
-      const apiDate = new Date(market.time * 1000).toTimeString();
+      const apiDate = new Date(market.time * 1000).toUTCString();
       message = 'This service is available every business day from 09:30 to 16:00 EDT.';
       marketStatus = ` is closed [${apiDate}]`;
     }
